fb1a3e7604ff147fb3f3210267484cc1c4c24162
Seyed moein Abtahi <100909323@oncampus.local>
2025-03-30 18:00:33 -0400

    Signed-off-by: Seyed moein Abtahi <100909323@oncampus.local>

---
+++ build/gcc/output/trcStaticBuffer.d
@@ -0,0 +1,106 @@
+output/trcStaticBuffer.o: \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/trcStaticBuffer.c \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcRecorder.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcDefines.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/TraceRecorderConfig/trcConfig.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/cmsis.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/SMM_MPS2.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/CMSDK_CM3.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/core_cm3.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/cmsis_version.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/cmsis_compiler.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/cmsis_gcc.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/mpu_armv7.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/TraceRecorderConfig/trcKernelPortConfig.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcTypes.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcHardwarePort.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/TraceRecorderConfig/trcStreamingConfig.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/TraceRecorderConfig/trcKernelPortStreamingConfig.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/kernelports/FreeRTOS/include/trcKernelPort.h \
+ /home/arampour/FreeRTOS-Kernel/include/FreeRTOS.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/FreeRTOSConfig.h \
+ /home/arampour/FreeRTOS-Kernel/include/projdefs.h \
+ /home/arampour/FreeRTOS-Kernel/include/portable.h \
+ /home/arampour/FreeRTOS-Kernel/include/deprecated_definitions.h \
+ /home/arampour/FreeRTOS-Kernel/portable/GCC/ARM_CM3/portmacro.h \
+ /home/arampour/FreeRTOS-Kernel/include/mpu_wrappers.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcHeap.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcString.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcStaticBuffer.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcError.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcEvent.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcEventBuffer.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcMultiCoreEventBuffer.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcTimestamp.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcEntryTable.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/streamports/RingBuffer/include/trcStreamPort.h \
+ /home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/TraceRecorderConfig/trcStreamPortConfig.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcISR.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcTask.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcObject.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcPrint.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcExtension.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcUtility.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcStackMonitor.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcInternalEventBuffer.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcDiagnostics.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcAssert.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcRunnable.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcDependency.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcProcess.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcThread.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcInterval.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcStateMachine.h \
+ /home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcCounter.h
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcRecorder.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcDefines.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/TraceRecorderConfig/trcConfig.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/cmsis.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/SMM_MPS2.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/CMSDK_CM3.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/core_cm3.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/cmsis_version.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/cmsis_compiler.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/cmsis_gcc.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/CMSIS/mpu_armv7.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/TraceRecorderConfig/trcKernelPortConfig.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcTypes.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcHardwarePort.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/TraceRecorderConfig/trcStreamingConfig.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/TraceRecorderConfig/trcKernelPortStreamingConfig.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/kernelports/FreeRTOS/include/trcKernelPort.h:
+/home/arampour/FreeRTOS-Kernel/include/FreeRTOS.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/FreeRTOSConfig.h:
+/home/arampour/FreeRTOS-Kernel/include/projdefs.h:
+/home/arampour/FreeRTOS-Kernel/include/portable.h:
+/home/arampour/FreeRTOS-Kernel/include/deprecated_definitions.h:
+/home/arampour/FreeRTOS-Kernel/portable/GCC/ARM_CM3/portmacro.h:
+/home/arampour/FreeRTOS-Kernel/include/mpu_wrappers.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcHeap.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcString.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcStaticBuffer.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcError.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcEvent.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcEventBuffer.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcMultiCoreEventBuffer.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcTimestamp.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcEntryTable.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/streamports/RingBuffer/include/trcStreamPort.h:
+/home/arampour/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/TraceRecorderConfig/trcStreamPortConfig.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcISR.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcTask.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcObject.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcPrint.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcExtension.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcUtility.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcStackMonitor.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcInternalEventBuffer.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcDiagnostics.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcAssert.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcRunnable.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcDependency.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcProcess.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcThread.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcInterval.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcStateMachine.h:
+/home/arampour/FreeRTOS/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/TraceRecorderSource/include/trcCounter.h:
